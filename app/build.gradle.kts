ed2aff45e8719159e4b5e601d5370b70effd750b
tata10180 <thaysbragaribeiro@gmail.com>
2026-08-05 18:21:43 -0300

    activity pt 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
--- app/src/main/java/com/thays/pokedex/MainActivity.kt
+++ app/src/main/java/com/thays/pokedex/MainActivity.kt
@@ -5,16 +5,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.thays.pokedex.databinding.ActivityMainBinding
+import android.view.View
+import android.content.Intent
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.loginButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.loginButton) {
+            val email = binding.emailField.text.toString()
+            val password = binding.passwordField.text.toString()
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                val intent: Intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Please enter both email and password", Toast.LENGTH_SHORT).show()
+            }
+            // Handle login logic here
+        }
     }
 }
+// ver com android qual eles acham melhor
+//when (v?.id) {
+//  R.id.loginButton -> {
+// Handle login button click
+//falta colocar um email+senha adm
